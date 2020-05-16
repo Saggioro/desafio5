2aeab1486c376564263ba4239164f52f858ffddc
Saggioro <diegosaggiorobrito@gmail.com>
2020-05-16 20:27:36 -0300

    desafio completo

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -15,14 +15,29 @@ class TransactionsRepository {
 
   public all(): Transaction[] {
     // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
     // TODO
+    let outcome = 0;
+    let income = 0;
+    this.transactions.forEach(transaction => {
+      if (transaction.type === 'income') {
+        income += transaction.value;
+      } else {
+        outcome += transaction.value;
+      }
+    });
+    const total = income - outcome;
+    return { income, outcome, total };
   }
 
-  public create(): Transaction {
+  public create({ title, type, value }: Omit<Transaction, 'id'>): Transaction {
     // TODO
+    const transaction = new Transaction({ title, type, value });
+    this.transactions.push(transaction);
+    return transaction;
   }
 }
 
--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -8,8 +8,22 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
+  public execute({ title, type, value }: Omit<Transaction, 'id'>): Transaction {
     // TODO
+    const balance = this.transactionsRepository.getBalance();
+    if (type === 'outcome') {
+      if (balance.total < value) {
+        throw Error('You havent enought money');
+      }
+    }
+
+    const transaction = this.transactionsRepository.create({
+      title,
+      type,
+      value,
+    });
+
+    return transaction;
   }
 }
 
